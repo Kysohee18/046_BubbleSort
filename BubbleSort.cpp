71f947c445d9155d3f6a8af522d675b754c87c73
Kyosohee18 <hanashahky@gmail.com>
2025-03-13 11:47:49 +0700

    display hasil tersusun

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -27,14 +27,42 @@ void input() {
             cin >> arr[i];
         } 
     }
-    void bubbleSortArray()
-    { int pass = 1; //step 1
+    void bubbleSortArray(){
+       int pass = 1; //step 1
 
         do
         { 
-            for
+            for (int j = 0; j <= n - 1 - pass; j++)
+            { //step2
+                if (arr[j] > arr [j + 1]) //step 3
+                {
+                    int temp;
+                    temp = arr[j];
+                    arr[j] = arr [j+1];
+                    arr[j + 1] = temp;
+                }
+            }
+            pass = pass + 1; //step 4
             
-        } while (condition);
+        } while (pass <= n - 1); //step 5
+
+        void display() {
+            cout << ednl;
+            cout << "==========================" << endl;
+            cout << "Element Array yang telah tersusun" << endl;
+            cout << endl;
+            for (int j = 0; j < n; j++) {
+                cout << arr[j]; //menampilkan array
+                if (j < n -1){
+                    cout << " -->";
+                }
+            }
+            cout << endl;
+            cout << endl;
+
+
+
+        }
         
 
 
@@ -53,8 +81,8 @@ void input() {
     
     }
 
-    }
+    
 
 
 
-    }
+    
